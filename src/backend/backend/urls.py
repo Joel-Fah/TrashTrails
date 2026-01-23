ca45d48e485f43d2e8e94cd2149aa4e935698b10
Joel-Fah <joelfah2003@gmail.com>
2026-01-23 12:14:53 +0100

    fixed workflow for django check

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -22,6 +22,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from core.views import health_check
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Trash Trails API",
